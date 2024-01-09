11d2157ac9d81dfdb46c0e5fb6782d0f57c262c8
Edvinas <edvinas.dr@gmail.com>
2024-01-09 16:55:57 +0200

    Added Terraform scripts for Kubernetes cluster creation and variables

--- scripts/resource-group.tf
+++ scripts/resource-group.tf
@@ -1,6 +1,10 @@
 # Creates a resource group for Vide Streaming in your Azure account.
 
-resource "azurerm_resource_group" "flixtube" {
-  name     = "flixtube"
-  location = "West Europe"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "edvdocker" {
+  name     = var.app_name
+  location = var.location
 }
